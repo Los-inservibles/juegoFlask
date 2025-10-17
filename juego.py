ff5d0d480490fa25c0e69fd01bc0a704106b668c
ElielISai <20243rd049@utez.edu.mx>
2025-10-16 22:41:29 -0600

    ruta get

--- juego.py
+++ juego.py
@@ -4,5 +4,9 @@ import string
 from datetime import datetime
 from flask import Flask, jsonify, request, render_template
 
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("index.html")
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
