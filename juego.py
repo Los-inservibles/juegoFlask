59c2e39ddab82f992449f6f1b38ca6a5fb75b4b4
ElielISai <20243rd049@utez.edu.mx>
2025-10-16 22:33:01 -0600

    archivo donde se va a trabajar

---
+++ juego.py
@@ -0,0 +1,8 @@
+import os
+import random
+import string
+from datetime import datetime
+from flask import Flask, jsonify, request, render_template
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
