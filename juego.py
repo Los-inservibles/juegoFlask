61a1957d1814c0945b1c5fd68722cfec24ce566b
ElielISai <20243rd049@utez.edu.mx>
2025-10-16 22:49:41 -0600

    funcion de iniciar juego y nuevo numero secreto

--- juego.py
+++ juego.py
@@ -8,5 +8,18 @@ from flask import Flask, jsonify, request, render_template
 def index():
     return render_template("index.html")
 
+@app.route("/start", methods=["POST"])
+def start_game():
+    "nuevo juego y numero"
+    game_id = "R" + "".join(random.choices(string.ascii_uppercase + string.digits, k=5))
+    secret = random.randint(1,100)
+    active_games[game_id] = {
+        "secret": secret,
+        "intentos": 0,
+        "puntaje": 0,
+        "creado_en": datetime.utcnow().isoformat() + "Z",
+    }
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
